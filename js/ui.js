9fae9338025e0ce91db16421610134d51411eb84
Александр Царёв <alex.carev1@icloud.com>
2026-03-20 14:14:16 +0300

    add basic game board layout and styles

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,13 @@
-console.log('ui module loaded');
+const board = document.getElementById('board');
+
+function renderStaticBoard() {
+  board.innerHTML = '';
+
+  for (let i = 0; i < 16; i++) {
+    const cell = document.createElement('div');
+    cell.className = 'cell';
+    board.appendChild(cell);
+  }
+}
+
+renderStaticBoard();
